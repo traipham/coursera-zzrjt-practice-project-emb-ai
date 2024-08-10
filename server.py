bab58945f6f633dd31fa8470cf8e69f0797a0bca
traipham <67611258+traipham@users.noreply.github.com>
2024-08-09 22:26:26 -0700

    Added BE routing and logic for sentiment analysis

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
-# Import Flask, render_template, request from the flask pramework package : TODO
-# Import the sentiment_analyzer function from the package created: TODO
+from flask import Flask, render_template, request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
-#Initiate the flask app : TODO
+app = Flask("Sentiment Analyzer")
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
@@ -14,15 +14,21 @@ def sent_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
+    text_to_analyse = request.args.get("textToAnalyze")
+    response_json = sentiment_analyzer(text_to_analyse)
+    if not response_json['label'] and not response_json['score']:
+        return "Invalid input! Try again."
+    label = response_json['label'][5:]
+    score = response_json['score']
+    res_text = f"The given text has been identified as {label} with a score of {score}."
+    return ({"msg":res_text}, 200)
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    app.run(debug=True)
